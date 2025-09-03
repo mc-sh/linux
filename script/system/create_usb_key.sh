e4b7f2d0874a9d5d2faf0f8685308a1ece3cd710
mc-sh <228965348+mc-sh@users.noreply.github.com>
2025-09-03 15:31:27 -0400

    create_usb

--- script/system/create_usb_key.sh
+++ script/system/create_usb_key.sh
@@ -10,11 +10,18 @@ echo "vim pour regarder et ajuster la commande en consequence"
 
 #sudo parted -s /dev/sdX mklabel msdos mkpart primary fat32 1MiB 8GiB (100%) set 1 boot on && sudo mkfs.vfat -F32 -n QFLASH /dev/sdX1
 
+#sudo wipefs -a /dev/sda && sudo parted -s -a optimal /dev/sda mklabel msdos mkpart primary fat32 1MiB 100% set 1 boot on && sudo mkfs.vfat -F32 -s 16 /dev/sda1
+
 #sudo fsck.vfat -v /dev/sda1
 #sudo stat -f /dev/sda1
 
 #cksum (donne ne resultat en decimal)
 
+#sudo dd if=~/Downloads/Parrot-security-6.4_amd64.iso of=/dev/sda bs=4M status=progress oflag=sync
+
+#sudo fdisk /dev/sdx (n for new partition)
+
+#sudo mkfs.ext4 -L persistence /dev/sdX3
+
 #diff -u <(sha512sum GIGABYTE.bin | awk '{print $1}') <(sha512sum ~/trash/GIGABYTE.bin | awk '{print $1}')
 
-#sudo wipefs -a /dev/sda && sudo parted -s -a optimal /dev/sda mklabel msdos mkpart primary fat32 1MiB 100% set 1 boot on && sudo mkfs.vfat -F32 -s 16 /dev/sda1
