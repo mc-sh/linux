4239f6f41f57bba1b76db7b55d19d3cd26e8cb52
mc-sh <228965348+mc-sh@users.noreply.github.com>
2025-09-05 23:10:52 -0400

    BUP

--- script/security/chip_me_verify.sh
+++ script/security/chip_me_verify.sh
@@ -1,4 +1,3 @@
-
 #guide to verify the security of the chip
 #and the integrity of intel me
 #need flashrom, uefitool, MEanalyser
@@ -11,3 +10,11 @@
 #find /usr/lib/live/mount/medium -type f \( -name "grub.cfg" -o -name "syslinux.cfg" -o -name "isolinux.cfg" \)
 
 #sudo flashrom -p internal -r full_spi.bin
+
+#extraire "as is" la region intel me et sauvegarder sous
+
+#analyse avec MEanalyser, identifier la version exacte, puis télécharger exactement la même version de intel ME sur le depot officiel de platomav (winraid.level1techs.com...) ensuite (mega.nz (cloud crypté))
+
+#diff -u <(xxd ~/Region_ME_ME_region.rgn) <(xxd ~/Downloads/8.1.2.1318_1.5MB_PRD_RGN.bin) > diff.txt
+
+#BUP ET FTPR doivent être isolé pour vérifier leur hash, car ce sont les deux seul élément qui ne sont pas changeant (vivant) C'EST LE CODE EXÉCUTABLE (le reste : log, nvram)
--- tools/read_before_usage.md
+++ tools/read_before_usage.md
@@ -1,4 +1,4 @@
-LES TOOL QUE ÇA CONCERNE : chipsec
+LES TOOL QUE ÇA CONCERNE : chipsec MEanalyser
 
 POUR UTILISER UNE COMMANDE INSTALLÉ DEPUIS GITHUB!!!!
 TOUJOURS COMMENCER PAR "source ~/venvs/$tool/bin/activate"
@@ -15,4 +15,12 @@ which python (doit etre le chemain du tool)
 cd ~/tools/$tool
 git pull
 pip install -e .
+POUR APPELER LE SCRIPT APRES INSTALLATION (PYTHON3 -M MEA.PY ~/SPI_ME.BIN)
 deactivate (pour get out venv)
+
+SI DOIT INSTALLÉ DÉPENDENCE
+. ~/venvs/$tool/bin/activate
+pip install -U pip #(pour updaté pip)
+SI PAS INSTALLÉ
+pip install -e . #(dans le dossier)
+pip install crccheck autrePaquet
