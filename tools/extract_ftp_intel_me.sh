cf837ec176eb5cccadfffeac89fa30aea68ed7ce
mc-sh <228965348+mc-sh@users.noreply.github.com>
2025-09-08 15:28:01 -0400

    Create extract_ftp_intel_me.sh

---
+++ tools/extract_ftp_intel_me.sh
@@ -0,0 +1,5 @@
+# extraction automatique des partitions (FTPR, NFTP, etc.)
+git clone https://github.com/skochinsky/me-tools
+cd me-tools
+python3 me_util.py /chemin/region_me.rgn -x
+# -> tu récupères FTPR_part.bin, NFTP_part.bin, etc.
