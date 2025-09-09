213c3785333bb08f5a77c630c831b20f2687c12e
mc-sh <228965348+mc-sh@users.noreply.github.com>
2025-09-09 15:24:03 -0400

    k

--- script/security/extract_tables.sh
+++ script/security/extract_tables.sh
@@ -8,6 +8,6 @@
 #POUR ROM-PARSER (VBIOS)
 #dd if=test0.rom of=legacy_vga.bin bs=1 skip=$((0x9200+0x18)) count=$((0x19000-0x9200-0x18))
 
-dd if=test0.rom of=efi_gop.bin bs=1 skip=$((0x19000+0x18)) count=$((TAILLE_TOTALE - 0x19000 - 0x18))
+#dd if=test0.rom of=efi_gop.bin bs=1 skip=$((0x19000+0x18)) count=$((TAILLE_TOTALE - 0x19000 - 0x18))
 
-Pour la deuxième commande, tu dois remplacer TAILLE_TOTALE par la taille du fichier (tu peux l’obtenir avec stat -c %s test0.rom).
+#Pour la deuxième commande, tu dois remplacer TAILLE_TOTALE par la taille du fichier (tu peux l’obtenir avec stat -c %s test0.rom).
